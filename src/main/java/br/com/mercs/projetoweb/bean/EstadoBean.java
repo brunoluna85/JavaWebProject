d1be3a59b3263e5203ca0597d0d94331423e176f
Bruno Luna Ribeiro <brunoluna85@gmail.com>
2016-09-20 12:29:35 -0300

    Criação do primeiro managed bean e uso de EL no xhtml.

---
+++ src/main/java/br/com/mercs/projetoweb/bean/EstadoBean.java
@@ -0,0 +1,12 @@
+package br.com.mercs.projetoweb.bean;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class EstadoBean {
+	
+	public void salvar(){
+		System.out.println("Programação web com Java");
+	}
+
+}
