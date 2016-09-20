cdfc5a7f6d7d0f7782a0b55b0551309693031cc6
Bruno Luna Ribeiro <brunoluna85@gmail.com>
2016-09-20 17:13:07 -0300

    Mensagem exibida através do bean de Estado.

--- src/main/java/br/com/mercs/projetoweb/bean/EstadoBean.java
+++ src/main/java/br/com/mercs/projetoweb/bean/EstadoBean.java
@@ -1,12 +1,19 @@
 package br.com.mercs.projetoweb.bean;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 
 @ManagedBean
 public class EstadoBean {
 	
 	public void salvar(){
-		System.out.println("Programação web com Java");
+		String texto = "Programação Web com Java";
+		System.out.println(texto);
+		FacesMessage mensagem = new FacesMessage(FacesMessage.SEVERITY_INFO,texto,texto);
+		
+		FacesContext contexto = FacesContext.getCurrentInstance();
+		contexto.addMessage(null, mensagem);
 	}
 
 }
